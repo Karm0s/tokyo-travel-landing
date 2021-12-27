69ab3dc59dc7099c49b8eba828c2ef2e35580376
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2021-12-27 02:11:05 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+	<div class="bg-blue-200 text-3xl">this is element</div>
+</template>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
