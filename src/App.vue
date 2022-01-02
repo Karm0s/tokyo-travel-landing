cbeba0aadcb98059fc4240227ed8f73a41151928
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2022-01-02 13:55:14 -0500

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue'
+
 </script>
 
 <template>
-	<HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-	<div class="bg-blue-200 text-3xl">this is element</div>
+	<section class="h-screen flex flex-col justify-between">
+		<Navbar></Navbar>
+		<div class="w-full flex justify-center items-center">
+			<div class="heading-wrapper bg-green-100">
+				<h1 class="text-8xl font-bold">Tokyo</h1>
+				<h2 class="text-2xl font-medium">... Not so far away</h2>
+			</div>
+			<div class="slideshow-wrapper bg-red-100">here slide show</div>
+		</div>
+		<div class="flex justify-between items-center">
+			<div class="infos">informations</div>
+			<div class="socials">socials</div>
+		</div>
+	</section>
 </template>
 
 <style>
 #app {
-	font-family: Avenir, Helvetica, Arial, sans-serif;
 	-webkit-font-smoothing: antialiased;
 	-moz-osx-font-smoothing: grayscale;
-	text-align: center;
-	color: #2c3e50;
-	margin-top: 60px;
+}
+* {
+	font-family: "Rubik";
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+	<nav class="flex justify-between items-center">
+		<div class="brand">
+			<h1>Tokyo</h1>
+		</div>
+		<div class="links w-1/2 flex justify-around">
+			<a href="">Home</a>
+			<a href="">About</a>
+			<a href="">Service</a>
+			<a href="">Package</a>
+			<a href="">Contact</a>
+			<a href="">Search</a>
+		</div>
+	</nav>
+</template>
+
+<style>
+</style>
