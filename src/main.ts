2d62611fc7bf96ca077bbb2caf3d3413f18eb213
Yanis Bouchenna <bouchenna.yanis@protonmail.com>
2022-01-04 14:03:20 -0500

    Install fontawesome icons

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-import { createApp } from "vue"
+import { createApp, VueElement } from "vue"
 import App from "./App.vue"
 import "./index.css"
 
-createApp(App).mount("#app")
+import { library } from "@fortawesome/fontawesome-svg-core"
+import { faUserSecret } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome"
+
+library.add(faUserSecret)
+
+createApp(App).component("font-awesome-icon", FontAwesomeIcon).mount("#app")
